51367f5c8474aeb2d377a91bbfad8995bfdebb20
Leon0ky <94325975+Leon0ky@users.noreply.github.com>
2024-07-05 02:11:54 +0300

    KAN-3: Another change to test Jira integration

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        // Another change for testing
+        // Another change for testing Jira integration with key KAN-3
     }
 }
