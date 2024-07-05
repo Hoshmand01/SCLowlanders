646985878ee9ee92f87230ef8db07fb9a06a005b
Leon0ky <94325975+Leon0ky@users.noreply.github.com>
2024-07-05 05:07:54 +0300

    Add HelloWorld Java project and JMeter test plan

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Another change for testing Jira integration with key KAN-3
+    }
+}
+"// Another change for testing Jira integration" 
