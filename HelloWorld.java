76bee9ece53610e63b616fc99e2ca9a105b0fa49
Leon0ky <94325975+Leon0ky@users.noreply.github.com>
2024-07-05 22:00:23 +0300

    Replace HelloWorld with Calculator

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        // Another change for testing Jira integration with key KAN-3
-        // Another change for testing Jira integration
-    }
-}
